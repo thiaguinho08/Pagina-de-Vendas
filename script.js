53b8c60a1f70d54c482213b21888486ef683c44c
thiaguinho08 <0000107482698xsp@al.educacao.sp.gov.br>
2024-12-04 18:11:20 -0300

    .

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 const cardcontainer = document.querySelector (".cardcontainer")
 const searchimput = document.querySelector("#searchimput")   
 
-const displayData = -> {
-    cardcontainer
+const DisplayData = -> {
+    cardcontainer .ImerHTML = + -;
+    Date.array.forEach(e => {
+        cardcontainer.ImerHTML + - 
+    });
 }
+
+<div class="card">
+
+    </div>
+
