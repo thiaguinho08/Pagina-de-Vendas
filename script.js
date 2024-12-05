d61fb184f080c7e1edaeaf9de034f96fcd435c13
thiaguinho08 <0000107482698xsp@al.educacao.sp.gov.br>
2024-12-05 15:10:33 -0300

    .

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 // script.js
 // Função para pesquisar produtos
- document.getElementById('pesquisa').addEventListener('input', function() {
+document.getElementById('pesquisa').addEventListener('input', function() {
     const termoPesquisa = this.value.toLowerCase();
     const produtos = document.querySelectorAll('.produto');
     produtos.forEach(produto => {
         const nomeProduto = produto.getAttribute('data-nome').toLowerCase();
         if (nomeProduto.includes(termoPesquisa)) {
             produto.style.display = 'block';
-    }
-    })
-} 
+
+
