0a8cfe75717287e2ee7f9d349359eca65f210d6b
thiaguinho08 <0000107482698xsp@al.educacao.sp.gov.br>
2024-12-05 16:10:31 -0300

    .

--- script.js
+++ script.js
@@ -11,3 +11,42 @@ function searchProducts() {
         }
     });
 }
+let carrinho = [];
+
+function adicionarAoCarrinho(item) {
+    // Adiciona o item ao carrinho
+    carrinho.push(item);
+    atualizarResumoCarrinho();
+}
+
+function atualizarResumoCarrinho() {
+    // Exibe os itens no carrinho
+    const cartSummary = document.getElementById('cart-summary');
+    if (carrinho.length > 0) {
+        cartSummary.innerHTML = `
+            <p>Itens no Carrinho: ${carrinho.join(', ')}</p>
+            <button class="cart-button" onclick="finalizarCompra()">Finalizar Compra</button>
+        `;
+    } else {
+        cartSummary.innerHTML = `
+            <p>Carrinho: Vazio</p>
+            <button class="cart-button" onclick="finalizarCompra()">Finalizar Compra</button>
+        `;
+    }
+}
+
+function finalizarCompra() {
+    // Redireciona para o site de compras (você pode substituir pela URL do seu checkout)
+    const urlCheckout = "https://www.seusite.com/checkout";
+    window.location.href = urlCheckout;
+}
+</script>
+
+
+    <script src="script.js"></script>
+</body>
+</html>
+ 
+
+<footer class="footer">
+    <p>&copy;Todos os direitos reservados.</p>
