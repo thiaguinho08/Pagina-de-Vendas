7fa2e287be5a854e9ee7d1abd9c4595a807ba4e9
thiaguinho08 <0000107482698xsp@al.educacao.sp.gov.br>
2024-12-12 14:34:58 -0300

    .

--- script.js
+++ script.js
@@ -23,7 +23,8 @@ inputBox.onkeyup = (e) => {
 
     // Se o usuário pressionar Enter, abrir a página do produto
     if (e.key === 'Enter' && userData) {
-        webLink = `https://www.seusite.com/produtos/${formattedProductName}`;
+        webLink = ``https://www.seusite.com/${categoria}`;
+
         window.open(webLink, '_blank');
     }
 
@@ -31,7 +32,8 @@ inputBox.onkeyup = (e) => {
     if (userData) {
         // Ao clicar no ícone, abrir o link do produto
         icon.onclick = () => {
-            webLink = https://www.seusite.com/produtos/${formattedProductName}`;
+            webLink =`https://www.seusite.com/${categoria}`;
+
             linkTag.setAttribute("href", webLink);
             linkTag.click();
         };
@@ -78,7 +80,8 @@ function select(element) {
     // Ao selecionar um item da lista, abrir o link correspondente
     let formattedProductName = selectData.replace(/\s+/g, '-').toLowerCase();
     icon.onclick = () => {
-        webLink =https://www.seusite.com/produtos/${formattedProductName}`;
+        webLink =`https://www.seusite.com/${categoria}`;
+
         linkTag.setAttribute("href", webLink);
         linkTag.click();
     };
